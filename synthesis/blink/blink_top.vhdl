8899a5a69b31164212dc14253e908600fe787def
JochiSt <jochen.steinmann@web.de>
2025-12-18 10:14:22 +0100

    included differential buffer for sys clk

--- synthesis/blink/blink_top.vhdl
+++ synthesis/blink/blink_top.vhdl
@@ -4,21 +4,31 @@ USE IEEE.STD_LOGIC_ARITH.ALL;
 
 ENTITY blink_top IS
     PORT (
-        SYS_CLK : IN STD_LOGIC;
-        LED     : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
-        SW      : IN STD_LOGIC_VECTOR(3 DOWNTO 0)
+        sys_clk_p : IN STD_LOGIC;
+        sys_clk_n : IN STD_LOGIC;
+        LED       : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+        KEY       : IN STD_LOGIC_VECTOR(3 DOWNTO 0)
     );
 END blink_top;
 
 ARCHITECTURE behaviour OF blink_top IS
+    SIGNAL sys_clk_200MHz : STD_LOGIC;  -- system clock 200MHz
 
 BEGIN
 
-    PROCESS (SYS_CLK) BEGIN
+    -- create differential clock buffer
+    IBUFGDS_0 : entity work.IBUFGDS
+    PORT MAP (
+        I => sys_clk_p,     -- normal input
+        IB => sys_clk_n,    -- inverted input
+        O => sys_clk_200MHz -- output
+    );
+
+    PROCESS (sys_clk_200MHz) BEGIN
         -- Process implementation goes here
-        IF rising_edge(SYS_CLK) THEN
+        IF rising_edge(sys_clk_200MHz) THEN
             -- Example logic: Toggle LEDs based on switches
-            LED <= SW;
+            LED <= KEY;
         END IF;
     END PROCESS;
 
