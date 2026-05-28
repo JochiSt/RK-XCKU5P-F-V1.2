6627a6593d3cd2e4e4ca9f8f3ddb352ff8af39f2
JochiSt <jochen.steinmann@web.de>
2026-05-28 20:18:41 +0200

    applied some fixed to made the liteeth core 100% compatible with ultrascale added prefix liteeth_core_0 to all clk signals added ignore to xdc, that pin is not available during IDELAYCTRL configuration

--- modules/Ethernet/RGMII/MakeLiteEthCore.mk
+++ modules/Ethernet/RGMII/MakeLiteEthCore.mk
@@ -4,6 +4,12 @@ all: liteeth_core.v liteeth_core.vhdl
 # generate liteeth core
 liteeth_core.v: XCKUP5P-RGMII.yaml
 	liteeth_gen --gateware-dir . --no-compile-software $<
+	# adapt for ULTRASCALE_PLUS architecture
+	sed -i -e 's/"ULTRASCALE"/"ULTRASCALE_PLUS"/g' liteeth_core.v
+	sed -i 's/\[get_nets /[get_nets liteeth_core_0\//g' liteeth_core.xdc
+	echo "" >> liteeth_core.xdc
+	echo "" >> liteeth_core.xdc
+	echo "set_property UNAVAILABLE_DURING_CALIBRATION TRUE [get_ports eth_txd[1]]" >> liteeth_core.xdc
 
 # generate VHDL instantiation template
 liteeth_core.vhdl: liteeth_core.v
